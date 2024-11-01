6aee4d3d143e580610dac0ff387da3a843628e6d
Elvin cooper <ing.elvin01cooper@gmail.com>
2024-11-01 19:44:10 -0400

    Creando modelo de usuarios y modelo del todo, con SQLAlchemy

--- todor/__init__.py
+++ todor/__init__.py
@@ -1,13 +1,22 @@
+import sqlalchemy
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
     # configurando el proyecto
     app.config.from_mapping(
         DEBUG = True,
-        SECRET_KEY = 'dev' # esta clave es solo para pruebas
+        SECRET_KEY = 'dev' ,# esta clave es solo para pruebas
+        SQLALCHEMY_DATABASE_URI = "sqlite:///todolist.db"
     )
+
+
+    # inicializar la conexion con la base de datos
+    db.init_app(app)
+
 
     # Registrar Blueprint del modulo todo
     from . import todo
@@ -19,6 +28,10 @@ def create_app():
      
     @app.route('/')
     def index():
-        return render_template('index.html') 
-    
+        return render_template('index.html')
+
+    with app.app_context():
+        db.create_all()
+
+
     return app
